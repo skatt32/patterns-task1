cda8e33ca513ce10364289c0d82a9718de8763f1
Neto test <test@neto.com>
2024-03-06 17:53:23 +0400

    Initial commit

--- src/main/java/ru/netology/delivery/data/DataGenerator.java
+++ src/main/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,10 +2,12 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
+import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
     private DataGenerator() {
